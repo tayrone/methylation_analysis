7038583830ed3724eac85c05adcc0c0fe36617f1
Tayrone Monteiro <tayronesm@gmail.com>
2020-10-08 09:21:56 -0300

    probe matching by analyses

--- 5_hyper_hipo_regulons.R
+++ 5_hyper_hipo_regulons.R
@@ -14,8 +14,6 @@ tfs <- names(rtna@listOfRegulons)
 load("./rdata_files/Group3/3_probewised.RData")
 
 
-
-
 methylation_results <- decideTests(fit, p.value = 0.01)
 methylation_results <- data.frame(methylation_results@.Data, 
                                   name = rownames(methylation_results@.Data),
@@ -29,46 +27,58 @@ methylation_results <- methylation_results %>%
   drop_na()
 
 
+length(unique(methylation_results$gene))
+length(intersect(unique(methylation_results$gene), probe_regulation$gene))
 
-methylation_results <- methylation_results[methylation_results$gene %in% tfs, ]
-
-
-#---- By summing up all results from fit model, define hipo and hyper 
-# methylated genes ----
-
-results <- decideTests(fit, p.value = 0.01)
-results <- data.frame(results@.Data, name = rownames(results@.Data),
-                      row.names = NULL)
-
-results <- select(results, name, up_down = control.Group3)
-
-results <- results %>% 
-  inner_join(diff_methylated, by = "name") %>% 
-  select(name, up_down, ucsc_refgene_name) %>% 
-  drop_na()
-
-results <- results %>% 
-  group_by(gene = ucsc_refgene_name) %>% 
-  transmute(sum = sum(up_down)) %>% 
-  distinct()
+# Since there are more genes in the methylation_results data frame, in 
+# comparison to probe_regulation, it is impossible to select the methylation
+# probes by comparing them to the expression regulation results. The next step 
+# might be to select all probes by their location on the gene, before doing
+# the investigation, for both expression and methylation analyses.
 
 
-ggplot(results) +
-  geom_tile()
-
-#---- Now, define portion of hipo and hyper methylation in each regulon ----
-
-function(regulon){
-  return(results[results$gene %in% regulon, ])
-  
-}
-
-genes_in_regulon <- 
-  lapply(regulons_list, function(regulon) results[results$gene %in% regulon, ])
 
 
-gdata::keep(hm_regulons, regulons_list, dm_genes, sure = T)
 
-sapply(regulons_list, function(x) sum(dm_genes %in% x))
+
+# methylation_results <- methylation_results[methylation_results$gene %in% tfs, ]
+# 
+# #---- By summing up all results from fit model, define hipo and hyper 
+# # methylated genes ----
+# 
+# results <- decideTests(fit, p.value = 0.01)
+# results <- data.frame(results@.Data, name = rownames(results@.Data),
+#                       row.names = NULL)
+# 
+# results <- select(results, name, up_down = control.Group3)
+# 
+# results <- results %>% 
+#   inner_join(diff_methylated, by = "name") %>% 
+#   select(name, up_down, ucsc_refgene_name) %>% 
+#   drop_na()
+# 
+# results <- results %>% 
+#   group_by(gene = ucsc_refgene_name) %>% 
+#   transmute(sum = sum(up_down)) %>% 
+#   distinct()
+# 
+# 
+# ggplot(results) +
+#   geom_tile()
+# 
+# #---- Now, define portion of hipo and hyper methylation in each regulon ----
+# 
+# function(regulon){
+#   return(results[results$gene %in% regulon, ])
+#   
+# }
+# 
+# genes_in_regulon <- 
+#   lapply(regulons_list, function(regulon) results[results$gene %in% regulon, ])
+# 
+# 
+# gdata::keep(hm_regulons, regulons_list, dm_genes, sure = T)
+# 
+# sapply(regulons_list, function(x) sum(dm_genes %in% x))
 
        
